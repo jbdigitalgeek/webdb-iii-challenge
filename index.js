6f1b2a877b4fd0f73d2600231eb1cfb17bd394d1
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-05 14:06:51 -0700

    debugged table making

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const helmet = require('helmet');
+
 const knex = require('knex');
-const knexConfig = require('./knexfile.js');
+const knexConfig = require('./knexfile');
 
 const db = knex(knexConfig.development);
 
 const server = express();
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+    try {
+        const cohorts = await db('cohorts');
+        res.status(200).json(cohorts);
+    } catch (error) {
+        res.status(500).json({ error: `${error}` });
+    }
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
